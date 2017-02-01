50b822d4f280679bf8d53ed3d97cd090f9be97c5
starlai21 <415156524@qq.com>
2017-02-01 14:30:33 -0400

    Task 1.1.1 Changed from contacts to todo items.

--- src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
@@ -27,9 +27,10 @@ public class ContactForm extends FormLayout {
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First name");
     TextField lastName = new TextField("Last name");
-    TextField phone = new TextField("Phone");
-    TextField email = new TextField("Email");
-    DateField birthDate = new DateField("Birth date");
+    TextField task = new TextField("task");
+    
+    DateField startDate = new DateField("Start Date");
+    DateField expectedEndDate = new DateField("Expected End Date");
 
     Contact contact;
 
@@ -60,7 +61,7 @@ public class ContactForm extends FormLayout {
         HorizontalLayout actions = new HorizontalLayout(save, cancel);
         actions.setSpacing(true);
 
-        addComponents(actions, firstName, lastName, phone, email, birthDate);
+        addComponents(actions, firstName, lastName,task,startDate,expectedEndDate);
     }
 
     /*
